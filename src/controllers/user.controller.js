2798043d388550b53044c58ec6766b57bb98fc60
Sourav Saha <saha83787@gmail.com>
2026-08-17 13:11:36 +0530

    login and logout added

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,8 +1,10 @@
 import { pool as db } from '../db/db.js';
 import { v4 as uuid } from 'uuid';
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
 
 export const createUser = async (req, res) => {
+    const { name, email, password } = req.body;
     try {
         await db.query(`
             CREATE TABLE IF NOT EXISTS users(
@@ -14,16 +16,52 @@ export const createUser = async (req, res) => {
         `);
 
         const userId = uuid();
-        const hash = await bcrypt.hash(req.body.password, 10);
+        const hash = await bcrypt.hash(password, 10);
         const insertSql = 'INSERT INTO users (id,name,email,password) VALUES (?,?,?,?)';
-        const [insertResult] = await db.execute(insertSql, [userId, req.body.name, req.body.email, hash]);
+        const [insertResult] = await db.execute(insertSql, [userId, name, email, hash]);
+
+        const token = jwt.sign({ id: userId }, process.env.JWT_SECRET);
+        res.cookie("token", token);
 
         res.status(200).json({ message: "New user created" });
     } catch (error) {
         console.error(error.message);
-        if(error.code === 'ER_DUP_ENTRY'){
-            return res.status(409).json({message:"User Already Exists"});
+        if (error.code === 'ER_DUP_ENTRY') {
+            return res.status(409).json({ message: "User Already Exists" });
         }
         res.status(500).json({ error: "Failed to create user" });
     }
+}
+
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const [existingUser] = await db.execute('SELECT * FROM users WHERE email = ?', [email]);
+
+        if (existingUser.length == 0) {
+            return res.status(409).json({ message: 'User is not registered with us please create an account' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(password, existingUser[0].password);
+
+        if (!isPasswordValid) {
+            return res.status(401).json({
+                message: "Invalid credentials"
+            });
+        }
+
+        const token = jwt.sign({ id: existingUser[0].id }, process.env.JWT_SECRET);
+
+        res.cookie("token", token);
+
+        res.status(200).json({ message: "User login successfull" });
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).json({ error: "Failed to login user" });
+    }
+}
+
+export const logoutUser = async (req, res) => {
+    res.clearCookie("token");
+    res.status(200).json({ message: "User logged out successfully" });
 }
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { createUser } from '../controllers/user.controller.js';
-import { validateCreateUser } from '../middlewares/validator.middleware.js'
+import { createUser, loginUser, logoutUser } from '../controllers/user.controller.js';
+import { validateCreateUser, validateLoginUser } from '../middlewares/validator.middleware.js'
 
 const router = express.Router();
 
 router.post('/register', validateCreateUser, createUser);
+
+router.post('/login', validateLoginUser, loginUser);
+
+router.post('/logout', logoutUser);
 
 export default router;

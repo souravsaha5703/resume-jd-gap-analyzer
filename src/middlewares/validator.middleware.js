4dba8bf00cacc151ee00f6012c5da419c3002524
Sourav Saha <saha83787@gmail.com>
2026-08-17 13:12:26 +0530

    login validator added

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -30,3 +30,15 @@ export const validateCreateUser = [
         .matches(/[@$!%*?&]/).withMessage('Password must contain at least one special character (@$!%*?&)'),
     validateResult
 ]
+
+export const validateLoginUser = [
+    body("email")
+        .trim()
+        .notEmpty().withMessage("Email is required")
+        .isEmail().withMessage("Please provide a valid email address")
+        .normalizeEmail(),
+    body('password')
+        .notEmpty().withMessage('Password is required')
+        .isLength({ min: 8 }).withMessage('Password must be at least 8 characters long'),
+    validateResult
+]

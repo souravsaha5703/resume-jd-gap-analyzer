31bdea3e9148473c3eeab06d74a303dafb17914f
Sourav Saha <saha83787@gmail.com>
2026-08-17 13:09:29 +0530

    jwt added

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import authRouter from './routes/auth.route.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api/auth', authRouter);
 
 app.get("/", (req, res) => {

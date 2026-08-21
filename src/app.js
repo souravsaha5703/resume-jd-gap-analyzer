b1d83dbd09fdb69f112c3e012ed2c2b9c33ab197
Sourav Saha <saha83787@gmail.com>
2026-08-21 21:42:57 +0530

    cors added

--- src/app.js
+++ src/app.js
@@ -3,15 +3,21 @@ import authRouter from './routes/auth.route.js';
 import resumeRouter from './routes/resume.route.js';
 import analysisRouter from './routes/analysis.route.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: ['http://localhost:5173'],
+    exposedHeaders: ['X-Data-Version'],
+    credentials: true
+}));
 app.use('/api/auth', authRouter);
-app.use('/api/resume',resumeRouter);
-app.use('/api/analysis',analysisRouter);
+app.use('/api/resume', resumeRouter);
+app.use('/api/analysis', analysisRouter);
 
 app.get("/", (req, res) => {
     res.status(201).json({ status: 200, message: "Server is healthy" });
